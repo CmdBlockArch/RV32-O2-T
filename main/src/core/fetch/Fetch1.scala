3886caa7b1826a4325054255cc4eb3c6cbfd3fa8
CmdBlock <54425466+CmdBlockZQG@users.noreply.github.com>
2025-03-11 18:15:16 +0800

    impl: ICache缺失回填

--- main/src/core/fetch/Fetch1.scala
+++ main/src/core/fetch/Fetch1.scala
@@ -2,23 +2,27 @@ package core.fetch
 
 import chisel3._
 import chisel3.util._
-
-import utils._
-import cache.CacheData
+import cache.{CacheData, CacheMeta}
 import conf.Conf.fetchWidth
 import conf.Conf.ICache._
+import perip.{AxiReadArb, MemReader}
 
 class Fetch1 extends Module {
-  import Fetch1._
-
   val in = IO(Flipped(new Fetch0.OutBundle))
   val out = IO(new Fetch.OutBundle)
   val dataRead = IO(new CacheData.ReadIO)
+  val metaWrite = IO(new CacheMeta.WriteIO)
+  val dataWrite = IO(new CacheData.WriteIO)
+  val arbRead = IO(new AxiReadArb.ReadIO)
   val io = IO(new Bundle {
     val ready = Output(Bool())
     val flush = Input(Bool())
   })
 
+  val memReader = Module(new MemReader(blockN))
+  arbRead :<>= memReader.arbRead
+
+  // 流水级寄存器
   val valid = RegInit(false.B)
   val cur = Reg(new Fetch0.OutBundle)
   val req = RegInit(false.B)
@@ -28,23 +32,51 @@ class Fetch1 extends Module {
     valid := true.B
     cur := in
   }
-  io.ready := !valid || cur.hit
+  io.ready := !req && (!valid || cur.hit)
 
+  // 读ICache
+  val tag = getTag(cur.pc)
   val index = getIndex(cur.pc)
   val offset = getOffset(cur.pc)
-
   dataRead.en := valid && cur.hit
   dataRead.index := getIndex(cur.pc)
   dataRead.way := cur.hitWay
 
+  // 输出指令读取结果
   assert(fetchWidth == 2)
   out.pc := cur.pc
   out.inst(0).valid := valid && cur.hit
   out.inst(0).inst := dataRead.data(offset)
   out.inst(1).valid := valid && cur.hit && offset =/= (blockN - 1).U
   out.inst(1).inst := dataRead.data(offset + 1.U)
-}
 
-object Fetch1 {
+  // 二路组相联的PLRU
+  assert(wayN == 2)
+  val plru = Reg(Vec(setN, UInt(1.W)))
+  when (valid && cur.hit) {
+    plru(index) := cur.hitWay
+  }
+  val evictWay = ~plru(index)
 
+  // ICache缺失回填
+  memReader.io.req := req
+  memReader.io.addr := Cat(tag, index, 0.U(offsetW.W))
+  when (valid && !cur.hit) {
+    req := true.B
+  }
+  when (memReader.io.resp) {
+    req := false.B
+    when (valid) { // 未发生flush
+      cur.hit := true.B
+      cur.hitWay := evictWay
+    }
+  }
+  metaWrite.en := memReader.io.resp
+  metaWrite.index := index
+  metaWrite.way := evictWay
+  metaWrite.data.tag := tag
+  dataWrite.en := memReader.io.resp
+  dataWrite.index := index
+  dataWrite.way := evictWay
+  dataWrite.data := memReader.io.data
 }
---
+++ main/src/perip/MemReader.scala
@@ -0,0 +1,46 @@
+package perip
+
+import chisel3._
+import chisel3.util._
+
+class MemReader(burstN: Int) extends Module {
+  val arbRead = IO(new AxiReadArb.ReadIO)
+  val io = IO(new Bundle {
+    // input
+    val req = Input(Bool())
+    val addr = Input(UInt(32.W))
+    // output
+    val resp = Output(Bool())
+    val data = Output(Vec(burstN, UInt(32.W)))
+  })
+
+  val single = burstN == 1
+  val burstW = log2Ceil(burstN)
+
+  val resp = RegInit(false.B)
+  val data = Reg(Vec(burstN, UInt(32.W)))
+
+  arbRead.setBurst(burstN)
+  arbRead.req := io.req
+  arbRead.addr := io.addr
+
+  val offset = if (single) 0.U
+  else RegInit(0.U(burstW.W))
+
+  when(arbRead.resp) {
+    data(offset) := arbRead.data
+    when (arbRead.last) {
+      resp := true.B
+      if (!single) offset := 0.U
+    } .otherwise {
+      if (!single) offset := offset + 1.U
+    }
+  }
+
+  when (resp) {
+    resp := false.B
+  }
+
+  io.resp := resp
+  io.data := data
+}
