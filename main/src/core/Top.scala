fc586d7f05876eccdfb34ddaffb68698a5f308a8
CmdBlock <54425466+CmdBlockZQG@users.noreply.github.com>
2025-03-11 19:41:37 +0800

    impl: 简单Fetch单元

--- main/src/core/Top.scala
+++ main/src/core/Top.scala
@@ -1,29 +1,32 @@
 package core
 
 import chisel3._
+import utils._
+import conf.Conf.debug
+import core.fetch.Fetch
+import perip.{AxiReadArb, AxiReadIO, AxiWriteArb, AxiWriteIO, SimMemRead, SimMemWrite}
 
-/** Compute GCD using subtraction method. Subtracts the smaller from the larger until register y is zero. value in
-  * register x is then the GCD
-  */
 class Top extends Module {
-  val io = IO(new Bundle {
-    val value1        = Input(UInt(16.W))
-    val value2        = Input(UInt(16.W))
-    val loadingValues = Input(Bool())
-    val outputGCD     = Output(UInt(16.W))
-    val outputValid   = Output(Bool())
-  })
+  val axiReadArb = Module(new AxiReadArb(1))
+  // val axiWriteArb = Module(new AxiWriteArb(1))
 
-  val x = Reg(UInt())
-  val y = Reg(UInt())
-
-  when(x > y) { x := x - y }.otherwise { y := y - x }
-
-  when(io.loadingValues) {
-    x := io.value1
-    y := io.value2
+  if (debug) {
+    val simMemRead = Module(new SimMemRead)
+    // val simMemWrite = Module(new SimMemWrite)
+    simMemRead.io :<>= axiReadArb.slave
+    // simMemWrite.io :<>= axiWriteArb.slave
+  } else {
+    val axiReadIO = IO(new AxiReadIO)
+    // val axiWriteIO = IO(new AxiWriteIO)
+    axiReadIO :<>= axiReadArb.slave
+    // axiWriteIO :<>= axiWriteArb.slave
   }
 
-  io.outputGCD   := x
-  io.outputValid := y === 0.U
+  val fetch = Module(new Fetch)
+  axiReadArb.master(0) :<>= fetch.arbRead
+  fetch.io.redirect := false.B
+  fetch.io.redirectPC := PC.resetPC
+
+  val testOut = IO(new Fetch.OutBundle)
+  testOut := fetch.out
 }
--- main/src/core/fetch/Fetch.scala
+++ main/src/core/fetch/Fetch.scala
@@ -1,12 +1,36 @@
 package core.fetch
 
 import chisel3._
-import chisel3.util._
 import conf.Conf.fetchWidth
+import core.fetch.cache.{CacheData, CacheMeta}
+import perip.AxiReadArb
 import utils._
 
 class Fetch extends Module {
+  val out = IO(new Fetch.OutBundle)
+  val arbRead = IO(new AxiReadArb.ReadIO)
+  val io = IO(new Bundle {
+    val redirect = Input(Bool())
+    val redirectPC = Input(PC())
+  })
 
+  val fetch0 = Module(new Fetch0)
+  val fetch1 = Module(new Fetch1)
+  val cacheMeta = Module(new CacheMeta)
+  val cacheData = Module(new CacheData)
+
+  fetch1.in := fetch0.out
+  cacheMeta.read :<>= fetch0.metaRead
+  fetch0.io.redirect := io.redirect
+  fetch0.io.redirectPC := io.redirectPC
+
+  out := fetch1.out
+  cacheData.read :<>= fetch1.dataRead
+  cacheMeta.write :<>= fetch1.metaWrite
+  cacheData.write :<>= fetch1.dataWrite
+  arbRead :<>= fetch1.arbRead
+  fetch0.io.ready := fetch1.io.ready
+  fetch1.io.flush := io.redirect
 }
 
 object Fetch {
